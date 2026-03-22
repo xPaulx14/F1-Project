39a2f8ee2a9c33ec0e956acdfa1d0d9805f6bba5
xPaulx14 <p.m.s.reif@mail.com>
2026-03-22 03:14:18 +0100

    navbar fix

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,19 @@
+document.getElementById('navbar-placeholder').innerHTML = `
+  <nav>
+    <ul class="navbar">
+      <li><a href="./index.html">Home</a></li>
+      <li><a href="./drivers.html">Drivers</a></li>
+      <li><a href="./tracks.html">Tracks</a></li>
+    </ul>
+  </nav>
+`;
+
 const currentPage = window.location.pathname;
-const navLinks = document.querySelectorAll('ul li a');
+const navLinks = document.querySelectorAll('.navbar li a');
 
 navLinks.forEach(link => {
-    if (link.getAttribute('href') === currentPage) {
-        link.classList.add('active-navbar');
-    }
+  const linkPath = link.getAttribute('href').replace('./', '/');
+  if (currentPage.endsWith(linkPath) || (currentPage.endsWith('/') && linkPath === '/index.html')) {
+    link.classList.add('active-navbar');
+  }
 });
